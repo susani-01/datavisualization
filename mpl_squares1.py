63317ea90bf4340e355154e3352dde02cb3f22cd
susani-01 <140640627+susani-01@users.noreply.github.com>
2023-11-15 00:34:07 +0900

    Add files via upload

---
+++ mpl_squares1.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+
+x_values = [1,2,3,4,5]
+y_values = [1,4,9,16,25]
+
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+ax.scatter(2.455,0.557,  s=200)
+
+# Set chart title and label axes.
+ax.set_title("square Numbers", fontsize = 24)
+ax.set_xlabel("Value", fontsize = 14)
+ax.set_ylabel("Square of Value", fontsize = 14)
+
+#set size of tick labels
+ax.tick_params(axis = "both",which = 'major', labelsize = 14)
+
+plt.show()
+  
+
+
+
+
+
+
+
+
+
